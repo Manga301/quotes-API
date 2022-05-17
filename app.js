1412413081ddd9eb79a0b36d1bade0beeb2f89d0
Mangaliso <mangaliso.twala@gmail.com>
2022-05-17 06:51:04 +0200

    Get quotes

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ app.use(express.urlencoded({
 }));
 
 app.use(express.static("public"));
+
+// connect to DB
+mongoose.connect("mongodb://localhost:27017/stoicQuotesDB", { useNewUrlParser: true });
+
+// schema
+const schemaQuotes = {
+    author: String,
+    quote: String,
+};
+
+// model
+const Quote = mongoose.model("Quote", schemaQuotes);
+
+
+app.route("/quotes")
+.get(function(req, res){
+
+    Quote.find(function(err, foundQuote){
+        if(!err){
+            res.send(foundQuote);
+        } else {
+            res.send(err);
+        }
+    });
+});
+
+app.listen(3000, function(){
+    console.log("Server started at port 3000.");
+});
